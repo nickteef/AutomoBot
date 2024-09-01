c392d7f0807930d315aa1dcd84627a875dad10ea
Nelit <76231385+nickteef@users.noreply.github.com>
2024-09-01 23:15:48 +0200

    Add files via upload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,76 @@
-const { config } = require("webpack");
-const { default: merge } = require("webpack-merge");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = merge(config, {
+module.exports = {
     mode: 'development',
-    devtool: 'inline-source-map'
-})
+    entry: {
+        popup: './src/popup.jsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true, // Omogoči CSS module
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][hash][ext]'
+                }
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/popup.html',
+            filename: 'popup.html',
+            chunks: ['popup']  // Določi, da ta HTML uporablja samo `popup` bundle
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "public", to: "" },
+            ],
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+        open: true,
+        watchFiles: ['src/**/*'],
+        historyApiFallback: {
+            index: 'popup.html'  // Nastavi privzeto datoteko, ki se odpre ob obisku
+        }
+    }
+};
